6ea30da7232f1a1fd9c72a46b9ecd36755d1d68a
Janetsun63 <76227320+Janetsun63@users.noreply.github.com>
2021-05-01 22:23:43 -0400

    update

--- hog.py
+++ hog.py
@@ -9,6 +9,7 @@ GOAL_SCORE = 100  # The goal of Hog is to score 100 points.
 # Phase 1: Simulator #
 ######################
 
+#test
 
 def roll_dice(num_rolls, dice=six_sided):
     """Simulate rolling the DICE exactly NUM_ROLLS > 0 times. Return the sum of
